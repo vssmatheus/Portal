4bad6c105461a7580a483040febe0ed62b3db11c
Gelzieny R. Martins <gelzieny@volus.com>
2020-07-31 16:50:17 -0300

    Alterando rota

--- PortalAgroDados/Portal/views/Autenticacao/url.py
+++ PortalAgroDados/Portal/views/Autenticacao/url.py
@@ -6,7 +6,7 @@ urlpatterns = [
     #AUTENTICA USUARIO
     path('Login', Login.login),
     path('', Login.login),
-    # path('Home', Login.login),
+    path('Home', Login.login),
     path('EsqueciMinhaSenha', Login.esqueci_minha_senha),
     path('AlteraSenha/<slug:token>', Login.altera_senha),
     path('Deslogar', Login.deslogar),
